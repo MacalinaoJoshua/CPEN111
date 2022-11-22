e8962c90f85df4897d693a77da84102fd6cc8e1e
MacalinaoJoshua <118825010+MacalinaoJoshua@users.noreply.github.com>
2022-11-22 21:55:22 +0800

    Update Exercise1.m

--- Exercise1.m
+++ Exercise1.m
@@ -1,10 +1,14 @@
-f = imread('C:\Users\Joshua\Desktop\CPEN 111\fruits.png');
-figure(1),imshow(f);
+#Macalinao, Joshua M.
+#BSCPE 4-2
+#CPEN 111
+
+fruits = imread('C:\Users\Joshua\Desktop\CPEN 111\fruits.png');
+figure(1),imshow(fruits);
 imfinfo 'C:\Users\Joshua\Desktop\CPEN 111\fruits.png'
 pkg load image;
-a = imresize(f,1/15);
-imwrite(a,'C:\Users\Joshua\Desktop\CPEN 111\fruits2.png');
-figure(2), imshow(a);
-b = rgb2hsv(f);
-imwrite(b,'C:\Users\Joshua\Desktop\CPEN 111\fruits3.png');
-figure(3), imshow(b);
+size = imresize(fruits,1/15);
+imwrite(size,'C:\Users\Joshua\Desktop\CPEN 111\fruits2.png');
+figure(2), imshow(size);
+color_hsv = rgb2hsv(fruits);
+imwrite(color_hsv,'C:\Users\Joshua\Desktop\CPEN 111\fruits3.png');
+figure(3), imshow(color_hsv);
